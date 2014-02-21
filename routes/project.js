2bc616ce84af92635de6b743165fcfd5921a6d42
vagrant <vagrant@precise32.(none)>
2014-02-21 22:34:18 +0000

    MongoDB lab

--- routes/project.js
+++ routes/project.js
@@ -2,9 +2,9 @@ var models = require('../models');
 
 exports.projectInfo = function(req, res) { 
   var projectID = req.params.id;
-
-  // query for the specific project and
-  // call the following callback
+  models.Project
+  .find('projectID')
+  .exec(afterQuery);
 
   function afterQuery(err, projects) {
     if(err) console.log(err);
@@ -12,17 +12,38 @@ exports.projectInfo = function(req, res) { 
   }
 }
 
+
+// form_data
 exports.addProject = function(req, res) {
   var form_data = req.body;
+  var newProject=new models.Project({"title": form_data.title , "image": form_data.image, "date": form_data.date , "summary": form_data.summary });
+    //newProject.update(form_data)
+    //{"title": form_data.title , "image": form_data.image, "date": form_data.date , "summary": form_data.summary })
   console.log(form_data);
+  newProject.save(afterSaving);
 
+ function afterSaving(err) { // this is a callback
+  if(err) {console.log(err); res.send(500); }
+  res.redirect('/');
+}
+ res.send();
   // make a new Project and save it to the DB
   // YOU MUST send an OK response w/ res.send();
 }
 
 exports.deleteProject = function(req, res) {
   var projectID = req.params.id;
+   models.Project
+  .find({'_id': projectID})
+  .remove()
+  .exec(afterRemovalMessage);
 
-  // find the project and remove it
-  // YOU MUST send an OK response w/ res.send();
+  function afterRemovalMessage(err){
+    if(err){
+      console.log(err);
+      res.send(500);
+    }
+    res.send();
+  }
+
 }
